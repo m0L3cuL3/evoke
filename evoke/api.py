328891434f0d092d8dbbe070da1e59a7539a83c1
Sean Baang <dev.seanbaang@gmail.com>
2024-04-08 13:47:54 +0800

    refactor: updated cash flow item column

--- evoke/api.py
+++ evoke/api.py
@@ -5,20 +5,14 @@ import frappe
 def get_days_of_month(date):
     # Convert input string to datetime object
     input_datetime = datetime.strptime(date, "%Y-%m-%d")
-    
-    # Get the year and month from the input date
+
+    # Get the year, month, and day from the input date
     year = input_datetime.year
     month = input_datetime.month
-    
-    # Calculate the number of days in the given month
-    num_days = (datetime(year, month % 12 + 1, 1) - timedelta(days=1)).day
-    
-    # Store all the days of the month in a list
-    days_of_month = []
-    for day in range(1, num_days + 1):
-        days_of_month.append(datetime(year, month, day).strftime("%Y-%m-%d"))
-    
-    return days_of_month
+    day = input_datetime.day
+
+    # Return the same date repeated 5 times
+    return [datetime(year, month, day).strftime("%Y-%m-%d")] * 5
 
 @frappe.whitelist()
 def get_month_and_year(date):
