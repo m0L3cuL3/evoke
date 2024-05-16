82fdbbdddb5432ddec370265e8a2f73e49a3c343
seanbaang <dev.seanbaang@gmail.com>
2024-05-16 09:52:51 +0000

    refactor: added highlight label for income and expenses

--- evoke/evoke/doctype/evoke_cash_flow/evoke_cash_flow.js
+++ evoke/evoke/doctype/evoke_cash_flow/evoke_cash_flow.js
@@ -4,6 +4,24 @@
 frappe.ui.form.on("Evoke Cash Flow", {
 	refresh: function (frm) {
 		frm.set_df_property("date", "read_only", frm.is_new() ? 0 : 1);
+		// frm.set_df_property("daily_entries", "cannot_add_rows", true);
+		frm.fields_dict["daily_entries"].$wrapper
+			.find(".grid-body .rows")
+			.find(".grid-row")
+			.each(function (i, item) {
+				let d =
+					locals[frm.fields_dict["daily_entries"].grid.doctype][
+						$(item).attr("data-name")
+					];
+
+				let transactionCell = $(item).find("[data-fieldname='transaction']");
+
+				if (d["transaction"] == "Income") {
+					transactionCell.css({ color: "limegreen" });
+				} else {
+					transactionCell.css({ color: "red" });
+				}
+			});
 	},
 	date: function (frm) {
 		let date = frm.doc.date;
@@ -32,25 +50,31 @@ frappe.ui.form.on("Evoke Cash Flow", {
 				});
 		}
 	},
+	add_single_row: function (frm) {
+		frm.add_child("daily_entries");
+		refresh_field("daily_entries");
+	},
 });
 
 frappe.ui.form.on("Daily Cash Flow Item", {
-	onload: function (frm) {},
-	refresh: function (frm) {
-		frm.fields_dict.transaction.$input.on("change", function () {
-			var selectedOption = $(this).val();
-			if (selectedOption === "Income") {
-				$(this).css("color", "var(--yellow-600)");
-			} else if (selectedOption === "Expenses") {
-				$(this).css("color", "var(--blue-600)");
-			}
-		});
-	},
-	transaction: function (frm) {
-		// frappe.meta.docfield_map["Select"].fieldtype.formatter = (value) => {
-		// 	console.log(value);
-		// 	if (value === "Income") return "🔵 Section Break";
-		// 	else return value;
-		// };
+	refresh: function (frm) {},
+	transaction: function (frm, cdt, cdn) {
+		frm.fields_dict["daily_entries"].$wrapper
+			.find(".grid-body .rows")
+			.find(".grid-row")
+			.each(function (i, item) {
+				let d =
+					locals[frm.fields_dict["daily_entries"].grid.doctype][
+						$(item).attr("data-name")
+					];
+
+				let transactionCell = $(item).find("[data-fieldname='transaction']");
+
+				if (d["transaction"] == "Income") {
+					transactionCell.css({ color: "limegreen" });
+				} else {
+					transactionCell.css({ color: "red" });
+				}
+			});
 	},
 });
